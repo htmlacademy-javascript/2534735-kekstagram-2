c8c71df9b678f045b94e1a3031df8e432823a3a3
Keks <keks@htmlacademy.ru>
2024-12-03 22:05:11 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/functions.js
+++ 3/js/functions.js
@@ -1,13 +1,18 @@
-function isLengthValid(str , maxLength) {
+function isLengthValid(str, maxLength) {
   return str.length <= maxLength;
 }
 
 function isPalindrome(str) {
-  const cleanedStr = str.replaceAll(' ', '');
+  const cleanedStr = str.toLowerCase().replaceAll(' ', '');
   const reversedStr = cleanedStr.split('').reverse().join('');
   return cleanedStr === reversedStr;
-  function toLowerCaseString(str) {
-    return str.toLowerCase();
-  }
 }
-isPalindrome('Топот');
+
+const example = "А роза упала на лапу Азора";
+const maxLength = 50;
+
+const lengthValid = isLengthValid(example, maxLength);
+console.log(`Is length valid: ${lengthValid}`);
+
+const palindromeCheck = isPalindrome(example);
+console.log(`Is palindrome: ${palindromeCheck}`);
